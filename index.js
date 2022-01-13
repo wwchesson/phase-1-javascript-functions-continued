d8c6fcbd220c27d7db04df337e6841f25cd375fb
Will <willchesson@gmail.com>
2022-01-13 18:19:15 -0500

    commit tree f79c94132a1f5a6f1f00a80e0102cdfe9b636235

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+
+  function wrapAdjective(punctuation="*") {
+    return function (msg="special") {
+        return `You are ${punctuation}${msg}${punctuation}!`;
+    }
+  }
